261d115bd9fcebe8f0a52ad1d0c0e1df9a60bf52
dagiLM10 <dagiberhanu19@gmail.com>
2024-04-19 06:38:26 -0700

    update

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 #This code kills a process && works together with the killme now file which has already been provided
-exec { 'killmenow':
-  command   =>  '/usr/bin/pkill killmenow',
+
+exec { 'pkill':
+  command   =>  'pkill killmenow',
   provider  =>  'shell',
-  returns   =>  [0, 1],
 }
