e2a660daa4c2ae10edcfd7481f0881896091f506
dagiLM10 <dagiberhanu19@gmail.com>
2024-04-20 01:53:57 -0700

    update

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Using puppet to connect without password
+
+file { '/etc/ssh/ssh_config':
+  ensure => present,
+}
+
+file_line { 'Turn off passwd auth':
+  path	=> '/etc/ssh/ssh_config',
+  line	=> 'PasswordAuthentication no',
+  match => '^#PasswordAuthentication',
+}
+
+file_line { 'Declare identify file':
+  path	=> '/etc/ssh/shh_config',
+  line  => 'IdentifyFile ~/.ssh/school',
+  match => '^#IdentityFile ~/.ssh/school',
+}
