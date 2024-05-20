d0efe8a66548bdc3089901a65bc0d7e6d8ce81cb
dagiLM10 <dagiberhanu19@gmail.com>
2024-05-20 02:53:07 -0700

    update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
-'''
-gather employee data from API
-'''
+"""
+Returns to-do list information for a given employee ID.
 
-import re
+This script takes an employee ID as a command-line argument and fetches
+the corresponding user information and to-do list from the JSONPlaceholder API.
+It then prints the tasks completed by the employee.
+"""
+
 import requests
 import sys
 
-REST_API = "http://jsonplaceholder.typicode.com"
 
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            id = int(sys.argv[1])
-            req = requests.get('{}/user/{}'.format(REST_API, id)).json()
-            task_req = requests.get('{}/todos'.format(REST_API)).json()
-            emp_name = req.get('name')
-            tasks = list(filter(lambda x: x.get('userId') == id, task_req))
-            completed_tasks = list(filter(lambda x: x.get('user.Id') == id, task_req))
-            print(
-                'Employee {} is done with tasks({}/{}):'.format(
-                emp_name,
-                len(completed_tasks),
-                len(tasks)
-                )
-            )
-            if len(completed_tasks) > 0:
-                for task in completed_tasks:
-                    print('\t {}'.format(task.get('title')))
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+
+    employee_id = sys.argv[1]
+    user = requests.get(url + "users/{}".format(employee_id)).json()
+
+    params = {"userId": employee_id}
+    todos = requests.get(url + "todos", params).json()
+
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+
+    [print("\t {}".format(complete)) for complete in completed]
