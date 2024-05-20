7c17c1b6d3f990071d9ae69498c24b28b21ddfa4
dagiLM10 <dagiberhanu19@gmail.com>
2024-05-20 02:33:33 -0700

    update

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == '__main__':
     user = sys.argv[1]
     url_user = 'https://jsonplaceholder.typicode.com/users/' + user
-    res = requests.get(url_user
+    res = requests.get(url_user)
     """ANYTHING"""
     user_name = res.json().get('username')
     task = url_user + '/todos'
